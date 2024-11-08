51cce857a95283a7ca78b303731349a0bdf7f422
Massimiliano Agostinoni <massimiliano@agostinoni.it>
2024-11-08 17:48:47 +0100

    bug fixing

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,7 @@
 import { application } from "app/framework/config/application";
 
 const languages = {};
+
 for (let i = 0; i < application.languages.supported.length; i++) {
   const lang = application.languages.supported[i];
   const file = await import("framework/languages/" + lang + ".mjs");
