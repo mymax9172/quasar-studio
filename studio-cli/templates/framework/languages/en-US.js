11c7251f7dfc3db885a49cf42c2ad3c39d175c40
mymax9172 <massimiliano@agostinoni.it>
2024-11-08 10:42:00 +0100

    Delete studio-cli directory

--- studio-cli/templates/framework/languages/en-US.js
+++
@@ -1,8 +0,0 @@
-// This is just an example,
-// so you can safely delete all default props below
-
-export const language = {
-  version: "Version",
-  customers: "Customers",
-  suppliers: "Suppliers",
-};
